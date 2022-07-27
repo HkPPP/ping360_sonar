1b4d0cd96565663d3a28abd1a0d6ded22b278749
Hank Pham <46695607+HkPPP@users.noreply.github.com>
2022-07-27 16:06:35 -0400

    added image viewer (#3)

---
+++ src/ping360_sonar/image_show.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import string
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class ImageShow:
+    def __init__(self, image_topic: string) -> None:
+        self.cv_bridge = CvBridge()
+        rospy.init_node("image_show", anonymous=True)
+
+        self.image_sub = rospy.Subscriber(image_topic, Image, self.callback)
+
+    def callback(self, data):
+        try:
+            cv_image = self.cv_bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        (rows, cols, channels) = cv_image.shape
+        if cols > 60 and rows > 60:
+            cv2.circle(cv_image, (50, 50), 10, 255)
+
+        cv2.imshow("Image window", cv_image)
+        cv2.waitKey(3)
+
+
+if __name__ == "__main__":
+    topic = "/ping360_node/sonar/images"
+    img_show = ImageShow(topic)
+    rospy.spin()
